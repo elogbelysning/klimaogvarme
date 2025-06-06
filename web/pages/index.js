d995fa37eb0e1fe20f6791dcaf5aae7f8932a2bc
elogbelysning <elogbelysningdk@gmail.com>
2025-06-06 16:19:23 +0200

    Create index.js

---
+++ web/pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Velkommen til klimaogvarme webshoppen!</h1>
+      <p>Her bliver der automatisk bygget en webshop med data fra AO.dk.</p>
+      <p>Du kan udvide denne side og hente data fra <code>../data/products.csv</code> senere.</p>
+    </main>
+  );
+}
